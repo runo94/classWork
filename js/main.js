097ba062547ac138d60ab5f87cabf818abd9fd37
Anton Kolesnykov <runo1194@gmail.com>
2021-11-08 21:07:02 +0200

    obj

--- js/main.js
+++ js/main.js
@@ -180,57 +180,66 @@ let users = [
 
 const contactList = [
     {
-      id: 1,
-      first_name: 'Vasya',
-      last_name: 'Petrov',
-      nick_name: '',
-      phone: 093945394,
-      group: [
-        {
-          name: 'friends',
-          id: 2
-        }
-      ],
-      actions: null
+        id: 1,
+        first_name: 'Vasya',
+        last_name: 'Petrov',
+        nick_name: '',
+        phone: 093945394,
+        group: [
+            {
+                name: 'friends',
+                id: 2
+            }
+        ],
+        actions: null
     },
     {
-      id: 2,
-      first_name: 'Petya',
-      last_name: '',
-      nick_name: 'Sizo',
-      phone: 09722122,
-      group: [
-        {
-          name: 'best_friends',
-          id: 1
-        }
-      ],
-      actions: [
-        {
-          name: 'missed call',
-          value: 3,
-          id: 1
-        }
-      ]
+        id: 2,
+        first_name: 'Petya',
+        last_name: '',
+        nick_name: 'Sizo',
+        phone: 09722122,
+        group: [
+            {
+                name: 'best_friends',
+                id: 1
+            }
+        ],
+        actions: [
+            {
+                name: 'missed call',
+                value: 3,
+                id: 1
+            }
+        ]
     },
     {
-      id: 2,
-      first_name: 'Rustem',
-      last_name: '',
-      nick_name: 'Box',
-      phone: 09700000,
-      group: [
-        {
-          name: 'other',
-          id: 3
-        }
-      ],
-      actions: [
-        {
-          name: 'missed call',
-          value: 13,
-          id: 1
-        }
-      ]
+        id: 3,
+        first_name: 'Rustem',
+        last_name: '',
+        nick_name: 'Box',
+        phone: 09700000,
+        group: [
+            {
+                name: 'other',
+                id: 3
+            }
+        ],
+        actions: [
+            {
+                name: 'missed call',
+                value: 13,
+                id: 1
+            }
+        ]
     }
 ];
+
+contactList.map(contact => {
+
+    let actName = contact.actions !== null ? contact.actions.map(action => `${action.name} ${action.value}`) : '';
+
+    console.log(
+        `Имя: ${contact.first_name} \n Псевдоним: ${contact.nick_name} \n Номер: ${Number(contact.phone)} \n ${actName}`
+    );
+})
