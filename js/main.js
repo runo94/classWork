622999f627586ee4ea208b9cd081c71378728bc2
Anton Kolesnykov <runo1194@gmail.com>
2021-11-22 21:06:09 +0200

    lesson

--- js/main.js
+++ js/main.js
@@ -1,23 +1,72 @@
-// function createDiv(atrName, atrVal) {
-//     let div = document.createElement('div');
-//     div.setAttribute(atrName, atrVal);
-//     return div;
-// };
+'use strict';
 
-// let root = document.getElementById('root');
 
-// root.appendChild(createDiv('class', 'first'));
-// root.appendChild(createDiv('class', 'sec'));
+// let hOne = document.getElementsByClassName('item');
 
-function sayHiBye(firstName, lastName) {
+// function test() {
+//     this.style.backgroundColor = '#ff0000'
+// }
 
-    function getFullName() {
-        return firstName + " " + lastName;
+// for (let i = 0; i < hOne.length; i++) {
+//     hOne[i].addEventListener('click', test)
+// }
+
+
+// console.log(this);
+
+
+// let some = () => {
+//     console.log(this);
+// }
+
+
+// function pow(x, n) {
+//     if (n == 1) {
+//         return x
+//     } else {
+//         console.log(n);
+//         return x * pow(x, n - 1)
+//     }
+// }
+
+// console.log(pow(2, 4));
+
+
+// let arr = [
+//     'asd',
+//     123,
+//     [213, 222, 'qwe']
+// ]
+
+
+// function newArr(array) {
+//     for (let i = 0; i < array.length; i++) {
+//         if (typeof array[i] === 'string') {
+//             array[i] = array[i] + 'y';
+//         } else if (Array.isArray(array[i])) {
+//             array[i] = newArr(array[i])
+//         }
+//     }
+
+//     return array;
+// }
+
+// console.log(newArr(arr));
+
+let arrTwo = [3, 10, 22, 11, 43, 123];
+let newArray = [];
+
+function someName(array, i = 0) {
+
+    if (i < array.length) {
+        if (array[i] % 2 === 0) {
+            newArray.push(array[i]);
+        }
+        someName(array, i + 1);
     }
+    return newArray;
 
-    alert("Hello, " + getFullName());
-    alert("Bye, " + getFullName());
 
 }
 
-sayHiBye('Anton', 'Kolesnykov')
+console.log(someName(arrTwo));
