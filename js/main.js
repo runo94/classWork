05ee00602bb220cfeacc7c5502254d4c2a7147b3
Anton Kolesnykov <runo1194@gmail.com>
2021-11-29 20:52:05 +0200

    lesson 8

--- js/main.js
+++ js/main.js
@@ -1,72 +1,47 @@
-'use strict';
+function main() {
+    console.log('A')
 
-
-// let hOne = document.getElementsByClassName('item');
-
-// function test() {
-//     this.style.backgroundColor = '#ff0000'
-// }
-
-// for (let i = 0; i < hOne.length; i++) {
-//     hOne[i].addEventListener('click', test)
-// }
-
-
-// console.log(this);
-
-
-// let some = () => {
-//     console.log(this);
-// }
-
-
-// function pow(x, n) {
-//     if (n == 1) {
-//         return x
-//     } else {
-//         console.log(n);
-//         return x * pow(x, n - 1)
-//     }
-// }
-
-// console.log(pow(2, 4));
-
-
-// let arr = [
-//     'asd',
-//     123,
-//     [213, 222, 'qwe']
-// ]
-
-
-// function newArr(array) {
-//     for (let i = 0; i < array.length; i++) {
-//         if (typeof array[i] === 'string') {
-//             array[i] = array[i] + 'y';
-//         } else if (Array.isArray(array[i])) {
-//             array[i] = newArr(array[i])
-//         }
-//     }
-
-//     return array;
-// }
-
-// console.log(newArr(arr));
-
-let arrTwo = [3, 10, 22, 11, 43, 123];
-let newArray = [];
-
-function someName(array, i = 0) {
-
-    if (i < array.length) {
-        if (array[i] % 2 === 0) {
-            newArray.push(array[i]);
-        }
-        someName(array, i + 1);
-    }
-    return newArray;
-
-
+    setTimeout(function exec() {
+        console.log('C')
+    }, 0)
+    setTimeout(function exec() {
+        console.log('B')
+    }, 0)
+    console.log('D')
 }
+main()
 
-console.log(someName(arrTwo));
+const pass = prompt('Enter PASS');
+
+const mapOfMarauder = new Promise(
+    (success, failed) => {
+        if (pass === 'Торжественно клянусь что замышляю только шалость') {
+            success('Господа Лунатик, Бродяга, Сохатый и Хвост! Поставщики вспомогательных средств для волшебников-шалунов с гордостью представляют свое новейшее изобретение КАРТУ МАРОДЕРОВ.')
+        } else {
+            failed('Господа Лунатик, Сохатый, Бродяга и Хвост приветствуют мистера Снейпа и требуют, что бы он не совал свой нос не в свои дела.')
+        }
+    })
+
+function askPass() {
+    mapOfMarauder
+   .then(result => {
+      console.log(result)
+    })
+   .catch(error => {
+      console.log(error)
+    })
+};
+
+askPass();
+   
+
+function name(params) {
+
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(() => reject(new Error("Ошибка!")), 1000);
+        setTimeout(() => resolve('SUCCESS'), 2000);
+    });
+    // .catch(f) это тоже самое, что promise.then(null, f)
+    promise.catch(console);
+    promise.then(console.log)
+}
