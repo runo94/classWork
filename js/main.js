0d7f21583c7aa4520c1449f1e0649af8ac25654e
Anton Kolesnykov <runo1194@gmail.com>
2021-12-13 20:51:57 +0200

    classWork

--- js/main.js
+++ js/main.js
@@ -1,55 +1,174 @@
-// 'use strict';
+
+// var a = 10;
+// let b = 20;
+
+// function pure (x, y) {
+//     return x+y
+// }
+
+// a = 30;
+// console.log(pure(a,b))
+// console.log(notpure());
+
+
+// function notpure() {
+//     return a+b
+// }
+
+// let array = [1,2,3,4,5,6,7,8]
+
+
+// function doubleD(arr) {
+//     return arr.map(n => n * 2)
+// }
+
+// console.log(doubleD([1]));
+
+// Array.prototype.map = function() {
+//   return [3]
+// }
+
+// console.log(doubleD([1]));
+// console.log(doubleD(array));
+
+// function User(name) {
+//     this.name = name;
+//     this.isAdmin = false;
+// }
+
+// let user = new User("Вася");
+// console.log(user);
+
+// function BigUser() {
+//     this.name = "Вася";
+//     return { name: "Godzilla" };
+// }
+
+// console.log(new BigUser().name);
+
+// function SmallUser() {
+//     this.name = "Вася";
+//     return;
+// }
+
+// console.log(new SmallUser().name);
+
+// function User(name) {
+//     this.name = name;
+//     this.sayHi = function () {
+//         alert("Меня зовут: " + this.name);
+//     };
+// }
+// let vasya = new User("Вася");
+
+// vasya.sayHi();
+
 
 // let animal = {
 //     eats: true
 // };
 
-// let rabbit = {
-//     jumps: true,
-//     __proto__: animal
+// function Rabbit(name) {
+//     this.name = name;
+// }
+
+// Rabbit.prototype = animal;
+// let rabbit = new Rabbit("White Rabbit");
+// console.log(rabbit);
+
+// let animal = {
+//     eats: true
 // };
 
-// let user = {
-//     type: 'human',
-//     breath: true,
-//     camputer: true
-// }
+// let rabbit = Object.create(animal);
+// console.log(rabbit.eats);
 
-// let moder = {
-//     muteAll: true,
-//     answersQ: true,
-//     __proto__: user
-// }
+// console.log(Object.getPrototypeOf(rabbit));
+// Object.setPrototypeOf(rabbit, {});
+// console.log(animal);
 
-// let  admin = {
-//     banAll: true,
-//     type: 'superman',
-//     __proto__: moder
-// }
-
-// for (let prop in user) {
-//     let isOwn = user.hasOwnProperty(prop);
-//     if (isOwn) {
-//         alert(`Our: ${prop}`);
-//     } else {
-//         alert(`Inherited: ${prop}`);
+// class User {
+//     constructor(name, email, password) {
+//         this.name = name;
+//         this.email = email;
+//         this.password = password;
+//     }
+//     sayHi = () => {
+//         const user = {
+//             email: '123@qq.xa'
+//         }
+//         if (this.email === user.email) {
+//             console.log(`Hi, ${this.name} !`);
+//         } else {
+//             console.log(`Who are u!?`)
+//         }
 //     }
 // }
 
-// // console.log(admin);
+// const userPetya = new User('Petya', 'peya11@qq.xa', '0000');
+// userPetya.sayHi();
 
+// console.log(User.prototype);
 
-function isEmpty(attr) {
-    if(typeof attr === 'object' && !Array.isArray(attr) ) {
-        console.log(Object.keys(attr));
-        return Object.keys(attr).length > 0 
+// class User {
+//     constructor(name, pw) {
+//         this.name = name;
+//         this.pw = pw;
+//     }
+
+//     get name() {
+//         return this._name;
+//     }
+
+//     get pw() {
+//         return this._pw
+//     }
+
+//     set pw(value) {
+//         if(String(value).length < 6) {
+//             console.error('TOOO SHORT!!!!!');
+//             return;
+//         }
+//         this._pw = value;
+//     }
+
+//     set name(value) {
+//         if (value.length < 4) {
+//             alert("Имя слишком короткое.");
+//             return;
+//         }
+//         this._name = value;
+//     }
+// }
+
+// let user = new User("Иван", 123);
+
+// console.log(user);
+
+// user.name = 'Jora'
+// user.pw = 121234;
+// console.log(user);
+
+let arr = [1, 2, 3, 4, 5]
+
+let res = arr.reduce((acc, curr) => acc + curr, 0);
+
+console.log(res);
+// 0 + 1
+// 1 + 2
+// 3 + 3
+// 6 + 4
+// 10 + 5
+// 15
+
+function outer () {
+    let x = 5
+    return function inner() {
+        x++
+        return x;
     }
 }
 
-let schedule = {};
+let a = outer();
 
-alert( isEmpty(schedule) ); // true
-
-schedule["8:30"] = "get up";
-
-alert( isEmpty(schedule) ); // false
+console.log(a);
