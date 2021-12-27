cf245ccd323e231ff747650a7697484bc28729ae
Anton Kolesnykov <runo1194@gmail.com>
2021-12-27 20:28:17 +0200

    ClassWork

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-'use strict';
-
 ///////////////////////////////////////////////////
 const URI = 'http://imtles.noodless.co.ua';
 const PER_PAGE = 5;
@@ -9,78 +7,146 @@ const PER_PAGE = 5;
 
 async function getPosts(page = 1) {
 
-  const postsDiv = document.getElementById('posts');
-  postsDiv.innerText = '';
+    const postsDiv = document.getElementById('posts');
+    postsDiv.innerText = '';
 
-  // запрашиваем JSON постов
-  const posts_response = await fetch(`${URI}` + '/wp-json/wp/v2/posts' + `?per_page=${PER_PAGE}` + `&page=${page}`); // http://imtles.noodless.co.ua/wp-json/wp/v2/posts?per_page=2&page=1
-  const posts_data = await posts_response.json();
+    // запрашиваем JSON постов
+    const posts_response = await fetch(`${URI}` + '/wp-json/wp/v2/posts' + `?per_page=${PER_PAGE}` + `&page=${page}`); // http://imtles.noodless.co.ua/wp-json/wp/v2/posts?per_page=2&page=1
+    const posts_data = await posts_response.json();
 
-  console.log(posts_data);
-  async function getImage(id) {
+    async function getImage(id) {
 
 
-    const img_response = await fetch(`${URI}` + '/wp-json/wp/v2/media/' + `${id}`); // http://imtles.noodless.co.ua/wp-json/wp/v2/media/7
-    const img_data = await img_response.json();
+        const img_response = await fetch(`${URI}` + '/wp-json/wp/v2/media/' + `${id}`); // http://imtles.noodless.co.ua/wp-json/wp/v2/media/7
+        const img_data = await img_response.json();
 
-    // console.log(img_data)
-
-    return img_data;
-  }
-
-  posts_data.map(async (postItem) => {
-
-    const placeholder = {
-      'media_details': {
-        'sizes': {
-          'thumbnail': {
-            'source_url': './img/avatar.jpg'
-          }
-        }
-      }
+        return img_data;
     }
 
-    const image = postItem.featured_media ? await getImage(postItem.featured_media) : placeholder;
+    posts_data.map(async (postItem, index) => {
 
-    const post = document.createElement('div');
-    const postTitle = document.createElement('h2');
-    postTitle.innerHTML = postItem.title.rendered;
+        const placeholder = {
+            'media_details': {
+                'sizes': {
+                    'thumbnail': {
+                        'source_url': './images/avatar.jpg'
+                    }
+                }
+            }
+        };
 
-    // const postImage = document.createElement('img');
-    // postImage.setAttribute('src', `${image.media_details.sizes.thumbnail.source_url}`);
-    const date = new Date(postItem.date);
+        const image = postItem.featured_media ? await getImage(postItem.featured_media) : placeholder;
 
-    const postText = document.createElement('p');
-    postText.innerHTML = postItem.content.rendered;
-    const postDate = document.createElement('p');
-    postDate.innerHTML = `${date.getDate()}. ${date.getMonth() + 1}. ${date.getFullYear()}`;
+        const post = document.createElement('div');
+        post.setAttribute('data-slide', index)
+        post.classList.add('post')
+        const postTitle = document.createElement('h2');
+        postTitle.innerHTML = postItem.title.rendered;
 
-    const postImage = document.createElement('img');
-    postImage.setAttribute('src', `${image.media_details.sizes.thumbnail.source_url}`);
-    post.appendChild(postImage);
+        const postText = document.createElement('p');
+        postText.innerHTML = postItem.content.rendered;
+        const postDate = document.createElement('p');
+        const subStr = postItem.date.substring(0, 9);
+        postDate.innerHTML = subStr;
 
-    postsDiv.appendChild(post);
-    post.appendChild(postTitle);
-    post.appendChild(postText);
-    post.appendChild(postDate);
+        const postImage = document.createElement('img');
+        postImage.setAttribute('src', `${image.media_details.sizes.thumbnail.source_url}`);
+        post.appendChild(postImage);
 
-    return postsDiv;
-  })
+        const btns = document.createElement('button');
+        btns.setAttribute('value', postItem.id);
+        btns.setAttribute('onclick', `getPost(${btns.value})`);
+        btns.innerText = 'click here';
 
-  return posts_data;
+        postsDiv.appendChild(post);
+        post.appendChild(postTitle);
+        post.appendChild(postText);
+        post.appendChild(postDate);
+        post.appendChild(btns)
+
+        return postsDiv;
+    })
+    const postsArr = document.getElementsByClassName('post');
+    postsArr[0].classList.add('active');
+    const nextBtn = document.getElementById('next');
+    const prevBtn = document.getElementById('prev');
+    nextBtn.addEventListener('click', () => {
+      const currEl = document.querySelector('.active');
+
+      if(posts.lastElementChild.getAttribute('data-slide') === currEl.getAttribute('data-slide')) {
+        postsArr[0].classList.add('active');
+        posts.lastElementChild.classList.remove('active')
+      }
+
+      currEl.nextElementSibling.classList.add('active');
+      currEl.classList.remove('active')
+    })
+    
+    prevBtn.addEventListener('click', () => {
+      const currEl = document.querySelector('.active');
+      console.log('currEl', postsArr.lastElementChild);
+      currEl.previousElementSibling.classList.add('active');
+      currEl.classList.remove('active')
+    })
+
+    return posts_data;
 }
-
 getPosts();
 
-async function getPost(id) {
-  
-  const container = document.getElementById('container');
-  // container.innerHTML = '';
 
-  const post_response = await fetch(`${URI}` + '/wp-json/wp/v2/posts/' + `${id}`); // http://imtles.noodless.co.ua/wp-json/wp/v2/posts/10
-  const post_data = await post_response.json();
 
-  console.log(post_data);
-}
+// async function getPost(id) {
 
-getPost(10)
+//     const post_response = await fetch(`${URI}` + '/wp-json/wp/v2/posts/' + `${id}`); // http://imtles.noodless.co.ua/wp-json/wp/v2/posts/10
+//     const post_data = await post_response.json();
+
+//     const placeholder = {
+//         'media_details': {
+//             'sizes': {
+//                 'thumbnail': {
+//                     'source_url': './images/avatar.jpg'
+//                 }
+//             }
+//         }
+//     };
+
+//     async function getImage(id) {
+
+
+//         const img_response = await fetch(`${URI}` + '/wp-json/wp/v2/media/' + `${id}`); // http://imtles.noodless.co.ua/wp-json/wp/v2/media/7
+//         const img_data = await img_response.json();
+
+//         return img_data;
+//     }
+//     const image = post_data.featured_media ? await getImage(post_data.featured_media) : placeholder;
+//     const thisP = document.getElementById('posts');
+//     console.log(thisP)
+//     thisP.innerHTML = '';
+//     const postImage = document.createElement('img');
+//     postImage.setAttribute('src', `${image.media_details.sizes.thumbnail.source_url}`);
+
+//     const post = document.createElement('div');
+//     const close = document.createElement('div');
+//     const closeImg = document.createElement('img')
+//     close.setAttribute('class', 'close_btn');
+//     closeImg.setAttribute('src', './images/close_btn.jpg');
+//     close.appendChild(closeImg)
+//     const postTitle = document.createElement('h2');
+//     postTitle.innerHTML = post_data.title.rendered;
+//     const postText = document.createElement('p');
+//     postText.innerHTML = post_data.content.rendered;
+//     const postDate = document.createElement('p');
+//     const subStr = post_data.date.substring(0, 9);
+//     postDate.innerHTML = subStr;
+
+
+//     thisP.appendChild(post);
+//     post.appendChild(close);
+//     post.appendChild(postImage);
+//     post.appendChild(postTitle);
+//     post.appendChild(postText);
+//     post.appendChild(postDate);
+
+//     close.addEventListener('click', () => getPosts)
+// }
+
